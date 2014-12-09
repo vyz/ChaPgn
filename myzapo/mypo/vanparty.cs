1016cff70e6b1f1173da9fc2ab50731b149ceaff
vyz <vyz@yandex.ru>
2014-12-09 17:07:18 +0400

    VanVariant
    Первый вариант для зацепки

---
+++ myzapo/mypo/vanparty.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mypo
+{
+    public class vanparty {
+        private string pgnDecs;
+        private List<string> allset;
+        private int iStartGameStoka;
+
+        public vanparty() { }
+
+        public vanparty(string opis, List<string> pset, int istart) {
+            pgnDecs = opis;
+            allset = pset;
+            iStartGameStoka = istart;
+            }
+
+        public void RussianRegExpSymbols() {
+            const string v1s = "o^";
+            const string v1t = "ю";
+            const string v2s = "~~";
+            const string v2t = "ч";
+            const string v3s = "><";
+            const string v3t = "Ч";
+            for (int i = iStartGameStoka; i < allset.Count; i++) {
+                string tt = allset[i].Replace(v1s, v1t);
+                tt = tt.Replace(v2s, v2t);
+                allset[i] = tt.Replace(v3s, v3t);
+                }
+            }
+
+        public List<string> Abzatz { get { return allset; } }
+        }
+}
---
+++ myzapo/myzapo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using mypo;
+
+namespace myzapo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string failo = Properties.Settings.Default.FiloIn;
+            PgnUtil worka = new PgnUtil();
+            List<vanparty> partyset = worka.FillListBoxWithDesc(PgnUtil.OpenInpFile(failo));
+            failo = Properties.Settings.Default.FiloOut;
+            StreamWriter wiw = new StreamWriter(failo, false, Encoding.UTF8);
+            foreach (vanparty aa in partyset)
+            {
+                aa.RussianRegExpSymbols();
+                foreach (string bb in aa.Abzatz)
+                {
+                    wiw.WriteLine(bb);
+                }
+            }
+            wiw.Close();
+        }
+    }
+}
